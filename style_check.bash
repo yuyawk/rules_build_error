4ed7d6328e159b2aa42ddb6dba37af56c53d3eca
yuyawk <46620009+yuyawk@users.noreply.github.com>
2024-04-10 00:44:46 +0900

    ci: Introduce `buildifier` style check (#10)

---
+++ style_check.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Code style checker (linter and formatter)
+
+set -euo pipefail
+
+# Bazel executable with some arguments
+BAZEL_EXECUTABLE=(
+    "env"
+    "-i"
+    BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1
+    BAZELISK_HOME=.cache/bazelisk
+    "HOME=${HOME}"
+    "PATH=${PATH}"
+    bazelisk
+)
+
+buildifier_targets="$(git ls-files *.bzl *.bazel BUILD WORKSPACE MODULE | xargs -I{} echo "$(pwd)/{}")"
+
+"${BAZEL_EXECUTABLE[@]}" run -- @buildifier_prebuilt//:buildifier -lint=fix ${buildifier_targets}
