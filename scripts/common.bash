f3db190166fc726b797cc875e029d877087c607d
yuyawk <46620009+yuyawk@users.noreply.github.com>
2024-04-13 06:41:15 +0900

    refactor: summarize common bash script implementations (#25)

---
+++ scripts/common.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Common prerequisites for manual bash execution
+
+set -euo pipefail
+
+# Absolute path of `scripts/` directory
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# Absolute path of the repository root
+REPO_ROOT_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+# Bazel executable with some arguments
+BAZEL_EXECUTABLE=(
+    "env"
+    "-i"
+    BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1
+    "BAZELISK_HOME=${REPO_ROOT_DIR}/.cache/bazelisk"
+    "HOME=${HOME}"
+    "PATH=${PATH}"
+    bazelisk
+)
+
+# Default Bazel version
+BAZEL_VERSION_DEFAULT="7.1.1"
+
+if [[ ! -f "${REPO_ROOT_DIR}/.bazeliskrc" ]]; then
+    echo "WARN: .bazeliskrc not found." >&2
+    echo "WARN: Creating it with a default version ${BAZEL_VERSION_DEFAULT}." >&2
+    echo "USE_BAZEL_VERSION=${BAZEL_VERSION_DEFAULT}" > "${REPO_ROOT_DIR}/.bazeliskrc"
+fi
