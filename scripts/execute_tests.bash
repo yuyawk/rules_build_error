4c60879ca696991963547f2bba76d75ed33dedf4
yuyawk <46620009+yuyawk@users.noreply.github.com>
2024-04-14 01:00:24 +0900

    test: add a test case for incorrect matcher (#33)

--- scripts/execute_tests.bash
+++ scripts/execute_tests.bash
@@ -41,3 +41,4 @@ check_bazel_build_error //tests/cc/cpp_successful_build_with_deps:plain
 check_bazel_build_error //tests/cc/cpp_successful_build_with_deps:with_basic_regex_matcher
 check_bazel_build_error //tests/cc/cpp_successful_build_with_deps:with_extended_regex_matcher
 check_bazel_build_error //tests/cc/cpp_successful_build_with_deps:with_substr_matcher
+check_bazel_build_error //tests/cc/cpp_compile_error:incorrect_matcher
