27a0985908cf633964a49119a4bc3045751650aa
yuyawk <h1gh5peedne6ula@gmail.com>
2024-04-13 12:23:43 +0900

    ci: Always explicitly specify Bazel version on CI

--- scripts/common.bash
+++ scripts/common.bash
@@ -25,6 +25,11 @@ BAZEL_EXECUTABLE=(
 BAZEL_VERSION_DEFAULT="7.1.1"
 
 if [[ ! -f "${REPO_ROOT_DIR}/.bazeliskrc" ]]; then
+    if [[ "${CI:-}" != "true" ]]; then
+        echo "ERROR: Explicitly specify Bazel version on CI" >&2
+        exit 1
+    fi
+
     echo "WARN: .bazeliskrc not found." >&2
     echo "WARN: Creating it with a default version ${BAZEL_VERSION_DEFAULT}." >&2
     echo "USE_BAZEL_VERSION=${BAZEL_VERSION_DEFAULT}" > "${REPO_ROOT_DIR}/.bazeliskrc"
