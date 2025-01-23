7b49c6e560038cb2151a9519b212338909f30360
yuyawk <h1gh5peedne6ula@gmail.com>
2025-01-23 22:34:59 +0900

    fix: style checker

--- development/cli/style_check.bash
+++ development/cli/style_check.bash
@@ -17,7 +17,7 @@ buildifier_targets="$(
 
 # Regarding native-cc:
 #   It's not required to load rules_cc at this moment.
-#   https://github.com/bazelbuild/buildtools/issues/923
+#   https://docs.google.com/document/d/1GW7UVo1s9X0cti9OMgT3ga5ozKYUWLPk9k8c4-34rC4/edit?tab=t.0#heading=h.9s188ie4hpe4
 "${BAZEL_EXECUTABLE[@]}" run -- \
     @buildifier_prebuilt//:buildifier \
         --lint=fix \
