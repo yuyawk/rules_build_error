a6b09ade247c11badbff72ec3428205d3af6d957
yuyawk <h1gh5peedne6ula@gmail.com>
2026-01-25 18:09:40 +0900

    modify test case to have catch-all

--- tests/cc/c_compile_error_matcher_using_select/c_compile_error.c
+++ tests/cc/c_compile_error_matcher_using_select/c_compile_error.c
@@ -3,21 +3,17 @@
 // Select the error message based on the target platform.
 
 #if defined(__linux__)
-#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is linux"
 #elif defined(_WIN32) || defined(_WIN64)
-#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is Windows"
 #elif defined(__APPLE__) && defined(__MACH__)
-#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is macOS"
 #else
-#define CONDITION 1
 #define ERROR_MESSAGE "The target platform is unknown"
 #endif
 
 int main()
 {
-    static_assert(CONDITION, ERROR_MESSAGE);
+    static_assert(0, ERROR_MESSAGE);
     return 0;
 }
