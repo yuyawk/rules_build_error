e92a7b53720429af0e8cfd8714a351d16fd4087b
yuyawk <h1gh5peedne6ula@gmail.com>
2026-01-25 17:55:13 +0900

    slightly modify the implementation

--- tests/cc/c_compile_error_matcher_using_select/c_compile_error.c
+++ tests/cc/c_compile_error_matcher_using_select/c_compile_error.c
@@ -2,16 +2,22 @@
 
 // Select the error message based on the target platform.
 
-#ifdef __linux__
+#if defined(__linux__)
+#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is linux"
 #elif defined(_WIN32) || defined(_WIN64)
+#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is Windows"
 #elif defined(__APPLE__) && defined(__MACH__)
+#define CONDITION 0
 #define ERROR_MESSAGE "The target platform is macOS"
+#else
+#define CONDITION 1
+#define ERROR_MESSAGE "The target platform is unknown"
 #endif
 
 int main()
 {
-    static_assert(0, ERROR_MESSAGE);
+    static_assert(CONDITION, ERROR_MESSAGE);
     return 0;
 }
