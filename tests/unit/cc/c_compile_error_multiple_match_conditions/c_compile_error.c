b601d21bb4c35334ba4a22c975c1a4b47b57ab36
yuyawk <h1gh5peedne6ula@gmail.com>
2026-02-05 20:12:28 +0900

    update readme

---
+++ tests/unit/cc/c_compile_error_multiple_match_conditions/c_compile_error.c
@@ -0,0 +1,7 @@
+#include <assert.h>
+
+int main()
+{
+    static_assert(0, "Multiple match conditions must be supported by this framework.");
+    return 0;
+}
--- tests/unit/cc/c_compile_error_multiple_matchers/c_compile_error.c
+++
@@ -1,7 +0,0 @@
-#include <assert.h>
-
-int main()
-{
-    static_assert(0, "Multiple matchers must be supported by this framework.");
-    return 0;
-}
